27ce4fe63d2234ae6a9208d6907b099c9128097c
MethmiNPCS <npcsmethmi@gmail.com>
2025-07-20 17:38:41 +0530

    Get a user by ID
    Backend logic for get user details with id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -75,6 +75,26 @@ exports.getAllUsers = async (req, res) => {
       res.status(500).json({ message: "Server error", error: err.message });
     }
   };
+
+  // Get a user by ID
+exports.getUserById = async (req, res) => {
+    try {
+      const userId = req.params.id;
+  
+      const user = await User.findById(userId).select("_id name email role xp level streak createdAt");
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.status(200).json({
+        message: "User fetched successfully",
+        user
+      });
+    } catch (err) {
+      res.status(500).json({ message: "Server error", error: err.message });
+    }
+  };
   
   
 // Update user profile
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getAllUsers, updateUser, deleteUser } = require("../controllers/UserController");
+const { registerUser, loginUser, getAllUsers, updateUser, deleteUser, getUserById } = require("../controllers/UserController");
 
 // Register Route
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
-router.get("/", getAllUsers);
+
 
 module.exports = router;
