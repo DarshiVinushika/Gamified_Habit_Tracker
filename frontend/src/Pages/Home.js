261c9a70eb8ed9e103cef90081abde0b79fcf99f
MethmiNPCS <npcsmethmi@gmail.com>
2025-07-27 22:03:08 +0530

    Small update

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../Components/Navbar";
-import Hero from "../Components/Hero";  
+import Footer from "../Components/Footer";
+import Hero from "../Components/Hero";
 
 const Home = () => {
   return (
-    <div className="min-h-screen"
-      style={{
-        background: "radial-gradient(closest-side at 50% 50%, #3B82F6 0%, #8B5CF6 100%)",
-    }}>
+    <div
+      className="flex flex-col min-h-screen"
+    >
+      <Navbar />
+      <main className="flex-grow md:ml-64">
         <Hero />
+      </main>
+      <Footer />
     </div>
   );
 };
